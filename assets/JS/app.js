fb9d605ce6cf247b4c00fedec757fe27096faf6c
freakysaran <saransaran32866@gmail.com>
2023-03-10 14:24:03 +0530

    Update app.js

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -21,27 +21,11 @@ function charLength() {
     return charLengths;
 }
 
+
 document.getElementById('length').addEventListener('change', charLength)
-function genarate() {
-    let saran=charLengths;
-    let password;
-    let output=" ";
-    let capital = document.getElementById("uppercase").checked
-    let arr = [...Array(26)].map((_, i) => String.fromCharCode(i + (capital ? 65 : 97)))
-    console.log(arr);
-}
 
-    
-    for( i=1; i<=saran; i++){
 
-    if(capital == true) {
-        const randomIndex = Math.floor(Math.random() * arr.length);
-        const item = arr[randomIndex];
-     password=item
-     output=output+password
-     document.getElementById('password__result').value = output;
-    }
-}
+
 
 
 
